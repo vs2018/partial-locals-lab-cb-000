5497870a03c9ed139e9b3ae4abdf3ef73cebbf0f
Vishal Sharma <vsh1819@gmail.com>
2018-07-20 12:19:18 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,28 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+  
+  def self.search(student_name)
+    student = student_name.downcase
+    if student_name == ''
+      return Student.all
+    else
+      Student.where("name like ?", "#{student}%")
+
+
+    end
+  end
 end
+
+# context 'when it receives an empty search argument' do
+#       it 'returns all students' do
+#         expect(Student.search('').count).to eq(Student.all.count)
+#       end
+#     end
+
+#     context 'when it receives a search query' do
+#       it 'returns all students whose names contain the given string' do
+#         expect(Student.search('rob').map {|s| s.name}).to match_array(['Robert', 'Roberta'])
+#       end
+#     end
+#   end
