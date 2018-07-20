e500e75542f30f2432af36e5f3dee0d8c3a8b2d9
Vishal Sharma <vsh1819@gmail.com>
2018-07-20 11:58:05 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -19,7 +19,7 @@ class Student < ActiveRecord::Base
     if student_name == ''
       return Student.all
     else
-      Student.where("name like ?", "%#{student}%")
+      Student.where("name like ?", "#{student}%")
 
 
     end
