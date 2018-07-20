8b0ba602fedfff055300e25ea5c3e7f8046f292a
Vishal Sharma <vsh1819@gmail.com>
2018-07-20 11:46:55 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -17,3 +17,16 @@ class Student < ActiveRecord::Base
   def search(student_name)
   end
 end
+
+context 'when it receives an empty search argument' do
+      it 'returns all students' do
+        expect(Student.search('').count).to eq(Student.all.count)
+      end
+    end
+
+    context 'when it receives a search query' do
+      it 'returns all students whose names contain the given string' do
+        expect(Student.search('rob').map {|s| s.name}).to match_array(['Robert', 'Roberta'])
+      end
+    end
+  end
